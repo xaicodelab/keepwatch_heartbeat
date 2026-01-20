f9da234b6b209ba9f09d9a015835eaf8b39092ea
xaicodelab <aicodelab@126.com>
2026-01-20 14:28:29 +0800

    去除没用到的页面和代码

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 // app.js
 App({
   onLaunch() {
+    // wx.getSetting({
+    //     success (res) {
+    //       console.log(res.authSetting);
+    //       if (!res.authSetting["scope.camera"]) {
+    //         res.authSetting = {
+    //           "scope.userInfo": true
+    //         };
+    //       }
+    //     }
+    // });
     // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // const logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    // wx.login({
+    //   success: res => {
+    //   }
+    // })
   },
   globalData: {
     userInfo: null
